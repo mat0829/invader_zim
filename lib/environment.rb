79fcc2538aa60f94daf3e2d62d0ebb942183475f
David Anderson <matandersonlv@hotmail.com>
2019-04-02 18:42:39 +0000

    updated gemspec stats

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,9 +1,10 @@
 require_relative "invader_zim/version"
 require_relative "invader_zim/cli"
-require_relative "invader_zim/charcter"
+require_relative "invader_zim/character"
 require_relative "invader_zim/scraper"
 
 
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
+require 'rmagick'
