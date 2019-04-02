911fb23d5597129bbd938f82fe9d653e54669fb3
David Anderson <matandersonlv@hotmail.com>
2019-04-02 18:08:09 +0000

    added to environment

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in invader_zim.gemspec
 gemspec
+
---
+++ lib/environment.rb
@@ -0,0 +1,9 @@
+require_relative "invader_zim/version"
+require_relative "invader_zim/cli"
+require_relative "invader_zim/charcter"
+require_relative "invader_zim/scraper"
+
+
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
