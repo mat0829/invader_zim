d83813a12cb8d8769f9cbc05b111dcc81f08aac6
David Anderson <matandersonlv@hotmail.com>
2019-04-15 21:18:09 +0000

    overhauled code again

--- lib/invader_zim/character.rb
+++ lib/invader_zim/character.rb
@@ -1,12 +1,15 @@
 class Character
-  attr_accessor :name, :debut, :profile_url
+  attr_accessor :name, :debut, :profile_url, :homeworld, :affiliation, :gender
   
   @@all = []
 
-  def initialize(name, debut, character_url)
+  def initialize(name = nil, debut = nil, character_url = nil, homeworld = nil, affiliation = nil, gender = nil)
      @name = name
      @debut = debut
      @character_url = character_url
+     @homeworld = homeworld
+     @affiliation = affiliation
+     @gender = gender
      @@all << self
   end
   
--- lib/invader_zim/scraper.rb
+++ lib/invader_zim/scraper.rb
@@ -8,16 +8,30 @@ class Scraper
 
     doc = Nokogiri::HTML(open(index_url))
       character_info = doc.css(".wikia-gallery-item")
-      character_array = []
     
       character_info.each do |character|
         name = character.css(".lightbox-caption center b a[href]").text,
         debut = character.css("[href]")[2].text,
-        character_url = "www.zim.fandom.com" + character.css("b a").map { |anchor| anchor["href"] }.first
+        character_url = "www.zim.fandom.com" + character.css("b a").first["href"]
+        
 
         Character.new(name, debut, character_url)
       end
+    end
+    
+    def self.scrape_profile_page
+      profile_url = "https://zim.fandom.com/wiki/Zim"
+      html = open(profile_url)
+      doc = Nokogiri::HTML(html)
+      character_table = doc.css(".infobox")
 
+      character_table.each do |table|
+        homeworld = table.css("a")[2].text,
+        affiliation = table.css("a[href]")[6].text,
+        gender = table.css("tr[6] td[2]").text.strip
+        
+        Character.new(homeworld, affiliation, gender)
+      end
     end
   
 end
