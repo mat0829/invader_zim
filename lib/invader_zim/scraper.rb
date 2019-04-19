828fcf52f18233c58da08e02fb8f6ba076fe1c8a
David Anderson <matandersonlv@hotmail.com>
2019-04-19 21:45:09 +0000

    added in new code to 2nd scraper which is now working! :)

--- lib/invader_zim/scraper.rb
+++ lib/invader_zim/scraper.rb
@@ -21,22 +21,34 @@ require 'open-uri'
       characters
     end
 
-  end
-    #def self.scrape_profile_page(character)
-    # html = open(character.profile_url)
-    # doc = Nokogiri::HTML(html)
-    # character_table = doc.css(".WikiaArticle")
-    #  character_page_traits = []
+    def self.scrape_profile_page(character)
+   html = open(character.profile_url)
+   doc = Nokogiri::HTML(html)
 
-    #  character_table.each do |table|
-    #  character_page_traits << {
-    #    :introduction => table.css("p")[2].text.strip.gsub(/[\"\n\u00A0]/, ' '),
-        
-    #    :appearence => table.css("p")[4].text.gsub(/[\"\n]/, ''),
-    #    :personality => table.css("p")[6].text.gsub(/[\"\n]/, '')
-    #  }
-    #  end
-    #  character_page_traits
-    #end
+   character_table = doc.css(".WikiaArticle")
+   character_page_traits = {} # NOW AN OBJECT NOT AN ARRAY! END GAME FOR THIS METHOD IS A HASH OF ATTRIBUTES, NOT AN ARRAY WITH A HASH OF ATTRIBUTES, NOR A HASH WITH ANOTHER HASH OF ATTRIBUTES
+
+   character_table.each do |table|
+     # character_page_traits << { # SO WE DON'T WANT TO PUT ANOTHER HASH INSIDE IT, REALLY
+       # :introduction => table.css("p")[2].text.strip.gsub(/[\"\n\u00A0]/, ' '),
+       # :appearence => table.css("p")[4].text.gsub(/[\"\n]/, ''),
+       # :personality => table.css("p")[6].text.gsub(/[\"\n]/, '')
+     # }
+
+
+
+  # INSTEAD, LET'S DIRECTLY ADD NEW KEY/VALUE PAIRS TO OUR NICE NEW CHARACTER_PAGE_TRAITS HASH
+
+     character_page_traits[:introduction] = table.css("p")[2].text.strip.gsub(/[\"\n\u00A0]/, ' ')
+     character_page_traits[:appearence] = table.css("p")[4].text.gsub(/[\"\n]/, '')
+     character_page_traits[:personality] = table.css("p")[6].text.gsub(/[\"\n]/, '') if table.css("p")[6]
+
+# LOOKS LIKE SOMETIMES THERE IS NO PERSONALITY p SO WE'LL CHECK TO SEE IF IT EXISTS - WE'LL ONLY ADD A :personality KEY IF THE p EXISTS ON THE WEB PAGE
+   end
+
+   character_page_traits
+
+ end
     
+end
   
