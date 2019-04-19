bd71724a9be2fc663a31b9cacf05f44a14188337
David Anderson <matandersonlv@hotmail.com>
2019-04-19 23:54:02 +0000

    updated cli code

--- lib/invader_zim/cli.rb
+++ lib/invader_zim/cli.rb
@@ -4,32 +4,8 @@ class InvaderZim::CLI
   def start
     make_characters
     add_attributes_to_characters
-    
     introduction
     show_characters
-  end
-  
-  
-  def introduction
-    CatpixMini::print_image "zim.gif",
-    :limit_x => 0.4,
-    :limit_y => 0,
-    :center_x => false,
-    :center_y => false,
-    :bg => "white",
-    :bg_fill => false,
-    :resolution => "high"
-    sleep(3)
-    puts CLIColorize.colorize("Welcome human! It is I, ZIM!", :red).strip
-    puts ""
-    sleep(4)
-    puts CLIColorize.colorize("If you are here then no doubt you know of my AMAZINGNESS and wish to learn more from me, ZIM!", :red)
-    puts ""
-    sleep(4)
-  
-    puts CLIColorize.colorize("Would you like to learn more about the amazingness that is me, ZIM, or a different, far less superior character?", :red)
-    puts ""
-    sleep(4)
   end
   
   def show_characters
@@ -52,6 +28,27 @@ class InvaderZim::CLI
     end
   end
   
+  
+  def introduction
+    CatpixMini::print_image "zim.gif",
+    :limit_x => 1,
+    :limit_y => 0,
+    :center_x => false,
+    :center_y => false,
+    :bg => "white",
+    :bg_fill => false,
+    :resolution => "high"
+    sleep(3)
+    puts CLIColorize.colorize("Welcome human! It is I, ZIM!", :red).strip
+    puts ""
+    sleep(4)
+    puts CLIColorize.colorize("If you are here then no doubt you know of my AMAZINGNESS and wish to learn more from me, ZIM!", :red)
+    puts ""
+    sleep(4)
+    puts CLIColorize.colorize("Would you like to learn more about the amazingness that is me, ZIM, or another, far less superior character?", :red)
+    puts ""
+    sleep(4)
+  end
 end
   
   
