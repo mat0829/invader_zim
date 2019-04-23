870b6368846c4f69f5ad666abd8cb5adbbb599c6
David Anderson <matandersonlv@hotmail.com>
2019-04-23 19:59:42 +0000

    checking code to make cleaner

--- lib/invader_zim/cli.rb
+++ lib/invader_zim/cli.rb
@@ -7,22 +7,6 @@ class InvaderZim::CLI
     menu
   end
   
-  def introduction
-    CatpixMini::print_image "zim.gif",
-    :limit_x => 1,
-    :limit_y => 0,
-    :center_x => false,
-    :center_y => false,
-    :bg => "white",
-    :bg_fill => false,
-    :resolution => "high"
-    sleep(3)
-    puts CLIColorize.colorize("Welcome human! It is I, ZIM!", :red).strip
-    sleep(4)
-    puts CLIColorize.colorize("If you are here then no doubt you know of my AMAZINGNESS and wish to learn more from me, ZIM!", :red)
-    sleep(4)
-  end
-  
   def menu
     puts CLIColorize.colorize("Would you like to learn more about the amazingness that is me, ZIM, or another, far less superior character?" , :red)
     sleep(5)
@@ -119,6 +103,22 @@ class InvaderZim::CLI
       attributes = Scraper.scrape_profile_page(character)
       character.add_character_attributes(attributes)
     #end
+  end
+  
+  def introduction
+    CatpixMini::print_image "zim.gif",
+    :limit_x => 1,
+    :limit_y => 0,
+    :center_x => false,
+    :center_y => false,
+    :bg => "white",
+    :bg_fill => false,
+    :resolution => "high"
+    sleep(3)
+    puts CLIColorize.colorize("Welcome human! It is I, ZIM!", :red).strip
+    sleep(4)
+    puts CLIColorize.colorize("If you are here then no doubt you know of my AMAZINGNESS and wish to learn more from me, ZIM!", :red)
+    sleep(4)
   end
   
   def show_characters_list
