17d3fe1fda65f25469992cdcb1406740cc709875
David Anderson <matandersonlv@hotmail.com>
2019-04-26 01:47:20 +0000

    removed notes for turning in for assessment

--- lib/invader_zim/scraper.rb
+++ lib/invader_zim/scraper.rb
@@ -22,33 +22,20 @@ require 'open-uri'
     end
 
     def self.scrape_profile_page(character)
-   html = open(character.profile_url)
-   doc = Nokogiri::HTML(html)
+      html = open(character.profile_url)
+      doc = Nokogiri::HTML(html)
 
-   character_table = doc.css(".WikiaArticle")
-   character_page_traits = {} # NOW AN OBJECT NOT AN ARRAY! END GAME FOR THIS METHOD IS A HASH OF ATTRIBUTES, NOT AN ARRAY WITH A HASH OF ATTRIBUTES, NOR A HASH WITH ANOTHER HASH OF ATTRIBUTES
+      character_table = doc.css(".WikiaArticle")
+      character_page_traits = {} 
 
-   character_table.each do |table|
-     # character_page_traits << { # SO WE DON'T WANT TO PUT ANOTHER HASH INSIDE IT, REALLY
-       # :introduction => table.css("p")[2].text.strip.gsub(/[\"\n\u00A0]/, ' '),
-       # :appearence => table.css("p")[4].text.gsub(/[\"\n]/, ''),
-       # :personality => table.css("p")[6].text.gsub(/[\"\n]/, '')
-     # }
+      character_table.each do |table|
 
-
-
-  # INSTEAD, LET'S DIRECTLY ADD NEW KEY/VALUE PAIRS TO OUR NICE NEW CHARACTER_PAGE_TRAITS HASH
-
-     character_page_traits[:information] ||= table.css(".infobox tr td").text.strip.gsub(/[\n]/, '-')
-     character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[0..8].text.strip.gsub(/[\"\n\t]/, '') if table.css(".mw-content-text ul li")[0..8]
-     #character_page_traits[:personality] = table.css("p")[6].text.gsub(/[\"\n]/, '') if table.css("p")[6]
-
-# LOOKS LIKE SOMETIMES THERE IS NO PERSONALITY p SO WE'LL CHECK TO SEE IF IT EXISTS - WE'LL ONLY ADD A :personality KEY IF THE p EXISTS ON THE WEB PAGE
-   end
-
-   character_page_traits
-
- end
+        character_page_traits[:information] ||= table.css(".infobox tr td").text.strip.gsub(/[\n]/, '-')
+        character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[0..8].text.strip.gsub(/[\"\n\t]/, '') if table.css(".mw-content-text ul li")[0..8]
+        #character_page_traits[:personality] = table.css("p")[6].text.gsub(/[\"\n]/, '') if table.css("p")[6]
+        end
+      character_page_traits
+    end
     
 end
   
