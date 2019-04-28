a3a464ee4d7310bdfc1f1a89bab4bc8f3e6a6f1d
David Anderson <matandersonlv@hotmail.com>
2019-04-28 04:23:15 +0000

    updated 2nd scraper

--- lib/invader_zim/scraper.rb
+++ lib/invader_zim/scraper.rb
@@ -33,7 +33,7 @@ require 'open-uri'
         #character_page_traits[:information] ||= table.css(".infobox tr td").text.strip.gsub(/[\n]/, '-')
           
           if character.name == "Roboparents" || character.name == "Recap Kid" || character.name == "Ms. Bitters"
-            character_page_traits[:introduction] ||= table.css("p")[0].text.strip.gsub(/[\n]/, ' ').gsub(/[\u00A0\u00E1\u2019]/, ' ').delete("\"")
+            character_page_traits[:introduction] ||= table.css("p")[0].text.strip.gsub(/[\n]/, ' ')
           elsif character.name == "Invader Skoodge"
             character_page_traits[:introduction] ||= table.css("p")[1].text.strip.gsub(/[\n]/, ' ').gsub(/[\u00A0\u00E1\u2019]/, ' ').delete("\"")
           elsif character.name == "Keef"
@@ -46,19 +46,21 @@ require 'open-uri'
           
           if character.name == "Keef"
             character_page_traits[:appearance] ||= table.css("p")[2].text.gsub(/[\"\n]/, '')
-          elsif character.name == "Dib Membrane" || character.name == "Ms. Bitters" || character.name == "Recap Kid" || character.name == "Invader Skoodge"
+          elsif character.name == "Dib Membrane" || character.name == "Recap Kid" || character.name == "Invader Skoodge" || character.name == "Minimoose"
             character_page_traits[:appearance] ||= table.css("p")[3].text.gsub(/[\"\n]/, '')
+          elsif character.name == "Gaz Membrane" || character.name == "Tak"
+            character_page_traits[:appearance] ||= table.css("p")[5].text.gsub(/[\"\n]/, '')  
           else
             character_page_traits[:appearance] ||= table.css("p")[4].text.gsub(/[\"\n]/, '').delete("\u00A0").delete("\u00E4").delete("\u00ED")
           end
           
-          if character.name == "Zim"
-            character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[5..10].text.strip.gsub(/[\"\n\t]/, '') #if table.css(".mw-content-text ul li")[5..10]
-          elsif
+          if character.name == "Minimoose"
             character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[0..5].text.strip.gsub(/[\"\n\t]/, '') 
               #if table.css(".mw-content-text ul li")[0..5]
+          elsif character.name == "Zim"
+            character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[5..10].text.strip.gsub(/[\"\n\t]/, '') #if table.css(".mw-content-text ul li")[5..10]
           else
-            character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[0..8].text.strip.gsub(/[\"\n\t]/, '')   #if table.css(".mw-content-text ul li")[0..8]
+            character_page_traits[:facts_of_doom] ||= table.css(".mw-content-text ul li")[0..8].text.strip.gsub(/[\"\n\t]/, '').delete("[1]").delete("[2]").delete("[3]")   #if table.css(".mw-content-text ul li")[0..8]
           end
           
         end
