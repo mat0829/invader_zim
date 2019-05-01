1d4c12bf306a27270cd56705f809ef95e6af92e0
David Anderson <matandersonlv@hotmail.com>
2019-05-01 21:22:06 +0000

    updated cli

--- lib/invader_zim/cli.rb
+++ lib/invader_zim/cli.rb
@@ -14,9 +14,12 @@ class InvaderZim::CLI
     sleep(3)
     
     puts CLIColorize.colorize("Type in your name so I can check if you are WORTHY of being friends with ZIM.\n" , :green).strip
+    
     input = gets.strip
     @human = InvaderZim::Human.new(input)
+    
     puts CLIColorize.colorize("\nIf you would like to skip the introduction type 'skip' and hit Enter or type any key and Enter to continue.\n" , :white).strip
+    
     input = gets.strip.downcase
     if input == "skip"
       system "clear"
@@ -131,11 +134,11 @@ class InvaderZim::CLI
        puts CLIColorize.colorize("Yay! I'm gunna sing The Doom Song now!\n", :yellow).strip
        #sleep(3)
        puts CLIColorize.colorize("Doom doom doom doom doom, doom doom do DOOM, DOOOM doom do-doom, DOOM do-doom doom doooom, doom doom dooom, do-do-DOOOM!\n", :yellow).strip
-       #sleep(5)
+       sleep(5)
        puts CLIColorize.colorize("(6 months later...)\n", :white).strip
-       #sleep(3)
+       sleep(3)
        puts CLIColorize.colorize("Doom doom doo doom doom, DOOMY-DOOMY-DOOM, doom do do DOOM, Do do DOOM, doomy-doomy-doomy, Doom doom doom THE END." , :yellow).strip
-       #sleep(6)
+       sleep(6)
        system "clear"
        if character.facts_of_doom == ""
          puts CLIColorize.colorize("Sorry #{@human.name}, this character has no Facts of Doom.", :red).strip
@@ -153,7 +156,7 @@ class InvaderZim::CLI
           system "clear"
           print_image("gir_red.gif")
           puts CLIColorize.colorize("\nThe knowledge, it fills me. It is neat.", :yellow).strip
-          #sleep(6)
+          sleep(6)
           menu
         elsif input == "n"
            system "clear"
@@ -175,7 +178,7 @@ class InvaderZim::CLI
        system "clear"
        print_image("ms_bitters.gif")
        puts CLIColorize.colorize("\n#{@human.name}, your performance was miserable! Your parents will receive a phone call instructing them to love you less.", :magenta).strip
-       #sleep(6)
+       sleep(6)
        menu
      else
        system "clear"
